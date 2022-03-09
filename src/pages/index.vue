f2c88be6415291fe742c3aa3c38c4a8763627baa
lucillem1313 <https://github.com/lucillem1313>
2022-03-09 17:57:39 -0600

    layout done

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+
+
+
+<template>
+  <div>
+    <input
+     
+      type="text"
+      placeholder="Search an Advise..."
+      class="w-full py-4 mt-16 text-xl text-center rounded-full"
+    />
+    <div class="grid grid-cols-2 gap-16 pt-16">
+      
+        <p>
+         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur illo deleniti ipsam atque sunt, corporis quod earum veritatis possimus provident illum ex minima, perferendis vero debitis excepturi libero, accusamus quibusdam?
+        </p>
+       <p>
+         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur illo deleniti ipsam atque sunt, corporis quod earum veritatis possimus provident illum ex minima, perferendis vero debitis excepturi libero, accusamus quibusdam?
+        </p>
+         <p>
+         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur illo deleniti ipsam atque sunt, corporis quod earum veritatis possimus provident illum ex minima, perferendis vero debitis excepturi libero, accusamus quibusdam?
+        </p>
+        
+    </div>
+  </div>
+</template>
--- src/router.js
+++ src/router.js
@@ -1,48 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from  "./pages/home.vue";
-import About from  "./pages/about.vue";
-import Services from  "./pages/services.vue";
-import NotFound from  "./pages/404.vue";
-import Help from  "./pages/help.vue";
+
+import Index from "./pages/index.vue";
+import NotFound from "./pages/404.vue";
 
 const routes = [
-    {
-path: "/home",
-name: "Home",
-component: Home,
-    },
-    {
-path: "/about",
-name: "About",
-component: About,
-    },
-    {
-
-path: "/services",
-name: "Services",
-component: Services,
-
-},
-{
-
-},
-{
-
-path: "/help",
-name: "Help",
-component: Help,
-
-},
-{
-path: "/:pathMatch(.*)*",
-name: "NotFound",
-component: NotFound,
-    },
+  {
+    path: "/",
+    name: "Index",
+    component: Index,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: NotFound,
+  },
 ];
 
 const router = createRouter({
- history: createWebHistory(),
- routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
